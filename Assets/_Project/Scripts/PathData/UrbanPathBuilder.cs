94580dc4005d99b9fb34f4d4aa797c82d7018b6f
Bartlomiej Stawiarz <extragracz@o2.pl>
2021-12-03 09:11:59 +0100

    Urban Path Builder methods extraction refactor.

--- Assets/_Project/Scripts/PathData/UrbanPathBuilder.cs
+++ Assets/_Project/Scripts/PathData/UrbanPathBuilder.cs
@@ -9,63 +9,88 @@ namespace PolSl.UrbanHealthPath.PathData
     {
         public UrbanPath Build()
         {
-            JsonObjectParser<HistoricalFact> historicalFactParser = new HistoricalFactJsonParser();
-            TextAsset historicalFactsFile = Resources.Load<TextAsset>("ExampleData/historical_facts");
-            string historicalFactsJson = historicalFactsFile.text;
+            List<HistoricalFact> historicalFacts = BuildHistoricalFacts("ExampleData/historical_facts");
+            List<Exercise> exercises = BuildExercises("ExampleData/exercises");
+            List<IWaypoint> waypoints = BuildWaypoints("ExampleData/waypoints", exercises, historicalFacts);
+            List<UrbanPath> paths = BuildUrbanPaths("ExampleData/urbanpath", waypoints);
 
-            JArray historicalFactsJArray = JsonConvert.DeserializeObject<JArray>(historicalFactsJson);
-            List<HistoricalFact> historicalFacts = new List<HistoricalFact>();
-            
-            foreach (JObject historicalFact in historicalFactsJArray)
-            {
-                historicalFacts.Add(historicalFactParser.Parse(historicalFact));
-            }
-            
-            JsonObjectParser<TextExerciseLevel> textExerciseLevelParser = new TextExerciseLevelJsonParser();
-            JsonObjectParser<ExerciseLevel> exerciseLevelParser = new ExerciseLevelJsonParser(textExerciseLevelParser);
-            JsonObjectParser<Exercise> exerciseParser = new ExerciseJsonParser(exerciseLevelParser);
-            
-            TextAsset exercisesFile = Resources.Load<TextAsset>("ExampleData/exercises");
-            string exercisesJson = exercisesFile.text;
+            return paths[0];
+        }
 
-            JArray exercisesJArray = JsonConvert.DeserializeObject<JArray>(exercisesJson);
-            List<Exercise> exercises = new List<Exercise>();
-            
-            foreach (JObject exercise in exercisesJArray)
-            {
-                exercises.Add(exerciseParser.Parse(exercise));
-            }
-            
-            JsonObjectParser<Station> stationParser = new StationJsonParser(exercises, historicalFacts);
-            JsonObjectParser<Waypoint> waypointParser = new WaypointJsonParser(stationParser);
-                
-            TextAsset waypointsFile = Resources.Load<TextAsset>("ExampleData/waypoints");
-            string waypointsJson = waypointsFile.text;
-
-            JArray waypointsJArray = JsonConvert.DeserializeObject<JArray>(waypointsJson);
-
-            List<IWaypoint> waypoints = new List<IWaypoint>();
-            
-            foreach (JObject waypoint in waypointsJArray)
-            {
-                waypoints.Add(waypointParser.Parse(waypoint));
-            }    
-                
+        private List<UrbanPath> BuildUrbanPaths(string filePath, List<IWaypoint> waypoints)
+        {
             JsonObjectParser<UrbanPath> urbanPathParser = new UrbanPathJsonParser(waypoints);
-            
-            TextAsset pathsFile = Resources.Load<TextAsset>("ExampleData/urbanpath");
+
+            TextAsset pathsFile = Resources.Load<TextAsset>(filePath);
             string pathsJson = pathsFile.text;
-            
+
             JArray pathsJArray = JsonConvert.DeserializeObject<JArray>(pathsJson);
-            
+
             List<UrbanPath> paths = new List<UrbanPath>();
-            
+
             foreach (JObject path in pathsJArray)
             {
                 paths.Add(urbanPathParser.Parse(path));
             }
 
-            return paths[0];
+            return paths;
+        }
+
+        private List<IWaypoint> BuildWaypoints(string filePath, List<Exercise> exercises, List<HistoricalFact> historicalFacts)
+        {
+            JsonObjectParser<Station> stationParser = new StationJsonParser(exercises, historicalFacts);
+            JsonObjectParser<Waypoint> waypointParser = new WaypointJsonParser(stationParser);
+
+            TextAsset waypointsFile = Resources.Load<TextAsset>(filePath);
+            string waypointsJson = waypointsFile.text;
+
+            JArray waypointsJArray = JsonConvert.DeserializeObject<JArray>(waypointsJson);
+
+            List<IWaypoint> waypoints = new List<IWaypoint>();
+
+            foreach (JObject waypoint in waypointsJArray)
+            {
+                waypoints.Add(waypointParser.Parse(waypoint));
+            }
+
+            return waypoints;
+        }
+
+        private List<Exercise> BuildExercises(string filePath)
+        {
+            JsonObjectParser<TextExerciseLevel> textExerciseLevelParser = new TextExerciseLevelJsonParser();
+            JsonObjectParser<ExerciseLevel> exerciseLevelParser = new ExerciseLevelJsonParser(textExerciseLevelParser);
+            JsonObjectParser<Exercise> exerciseParser = new ExerciseJsonParser(exerciseLevelParser);
+
+            TextAsset exercisesFile = Resources.Load<TextAsset>(filePath);
+            string exercisesJson = exercisesFile.text;
+
+            JArray exercisesJArray = JsonConvert.DeserializeObject<JArray>(exercisesJson);
+            List<Exercise> exercises = new List<Exercise>();
+
+            foreach (JObject exercise in exercisesJArray)
+            {
+                exercises.Add(exerciseParser.Parse(exercise));
+            }
+
+            return exercises;
+        }
+
+        private List<HistoricalFact> BuildHistoricalFacts(string filePath)
+        {
+            JsonObjectParser<HistoricalFact> historicalFactParser = new HistoricalFactJsonParser();
+            TextAsset historicalFactsFile = Resources.Load<TextAsset>(filePath);
+            string historicalFactsJson = historicalFactsFile.text;
+
+            JArray historicalFactsJArray = JsonConvert.DeserializeObject<JArray>(historicalFactsJson);
+            List<HistoricalFact> historicalFacts = new List<HistoricalFact>();
+
+            foreach (JObject historicalFact in historicalFactsJArray)
+            {
+                historicalFacts.Add(historicalFactParser.Parse(historicalFact));
+            }
+
+            return historicalFacts;
         }
     }
 }
