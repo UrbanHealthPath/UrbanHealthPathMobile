2b38967fbdc3ec9cd6672aba9ec99f93193efd5d
PaulinaCzapla <paulina.czp00@gmail.com>
2021-12-17 08:31:17 +0100

    Added path choice view.

---
+++ Assets/_Project/Scripts/UserInterface/PathChoiceView.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PolSl.UrbanHealthPath.UserInterface
+{
+    public class PathChoiceView : MonoBehaviour, IDisplayable
+    {
+        [SerializeField] private Button menuButton, tmpChoiceButton;
+
+        public void Start()
+        {
+            menuButton.onClick.AddListener(GoToMainMenu);
+            tmpChoiceButton.onClick.AddListener(StartPath);
+        }
+
+        public void Display()
+        {
+            this.enabled = true;
+            this.gameObject.SetActive(true);
+        }
+
+        public void StopDisplay()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        private void Return()
+        {
+            ViewManager.GetInstance().OpenView(ViewManager.GetInstance().LastViewType);
+        }
+        private void StartPath()
+        {
+            ViewManager.GetInstance().OpenView(ViewType.Path);
+        }
+
+        private void GoToMainMenu()
+        {
+            ViewManager.GetInstance().OpenView(ViewType.Main);
+        }
+
+        public void OnDestroy()
+        {
+            menuButton.onClick.RemoveListener(GoToMainMenu);
+            tmpChoiceButton.onClick.RemoveListener(StartPath);
+        }
+    }
+}
