436a59619443a1de16279a95508eefa360fd9c41
Bartlomiej Stawiarz <extragracz@o2.pl>
2022-01-02 20:26:40 +0100

    Logging implementation.

---
+++ Assets/_Project/Scripts/Tools/TextLogger/ITextLogger.cs
@@ -0,0 +1,8 @@
+﻿namespace PolSl.UrbanHealthPath.Tools.TextLogger
+{
+    public interface ITextLogger
+    {
+        void Log(LogVerbosity verbosity, string message);
+        void Log(LogVerbosity verbosity, string category, string message);
+    }
+}
---
+++ Assets/_Project/Scripts/Tools/TextLogger/LogVerbosity.cs
@@ -0,0 +1,11 @@
+﻿namespace PolSl.UrbanHealthPath.Tools.TextLogger
+{
+    public enum LogVerbosity
+    {
+        Debug = 0,
+        Log = 10,
+        Warning = 100,
+        Error = 1000,
+        Fatal = 10000,
+    }
+}
