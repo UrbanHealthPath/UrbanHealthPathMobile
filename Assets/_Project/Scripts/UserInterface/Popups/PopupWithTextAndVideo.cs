1d1d5edef61d788a431eeda06e348eb1675c30f3
Paulina Czapla <paulina.czp00@gmail.com>
2022-01-10 22:04:33 +0100

    Created popup with text and video.

---
+++ Assets/_Project/Scripts/UserInterface/Popups/PopupWithTextAndVideo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using PolSl.UrbanHealthPath.UserInterface.Components;
+using PolSl.UrbanHealthPath.UserInterface.Initializers;
+using PolSl.UrbanHealthPath.UserInterface.Interfaces;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace PolSl.UrbanHealthPath.UserInterface.Popups
+{
+    public class PopupWithTextAndVideo : MonoBehaviour, IPopup, IInitializable
+    {
+        public RectTransform PopupArea => popupArea;
+
+        [SerializeField] private Header header;
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private RectTransform popupArea;
+        [SerializeField] private VideoPlayer videoPlayer;
+
+        public void Initialize(Initializer initializer)
+        {
+            if (initializer is PopupWithTextAndVideoInitializer init)
+            {
+                header.Initialize(init.HeaderText);
+                text.text = init.Text;
+
+                videoPlayer.clip = init.Clip;
+                videoPlayer.Play();
+
+                InitSizeAndPosition(init.Payload);
+            }
+        }
+
+        public void InitSizeAndPosition(PopupPayload payload)
+        {
+            PopupArea.sizeDelta = new Vector2(payload.Size.x, payload.Size.y);
+            PopupArea.transform.position = new Vector2(payload.Position.x, payload.Position.y);
+        }
+    }
+}
