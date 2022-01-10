d0046581b4d4e0860847dad453de8c0ec4c4f7e6
Paulina Czapla <paulina.czp00@gmail.com>
2022-01-10 21:58:54 +0100

    AppearanceChangingButton is now more generic.

--- Assets/_Project/Scripts/UserInterface/Components/AppearanceChangingButton.cs
+++ Assets/_Project/Scripts/UserInterface/Components/AppearanceChangingButton.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace PolSl.UrbanHealthPath.UserInterface.Components
@@ -16,52 +15,65 @@ namespace PolSl.UrbanHealthPath.UserInterface.Components
             [SerializeField] private Sprite sprite;
             [SerializeField] private String text;
             [SerializeField] private int bottomOffset;
-            
-            public Sprite  GetSprite()
+
+            public Sprite GetSprite()
             {
                 return sprite;
             }
-        
+
             public String GetText()
             {
                 return text;
             }
-            
+
             public int GetOffset()
             {
                 return bottomOffset;
             }
         }
-        
-        [SerializeField] private List<ButtonAppearance> _buttonAppearances;
-        
-        [SerializeField] private Image _image;
-        
-        [SerializeField] private TextMeshProUGUI _text;
-        
+
+        [Tooltip("List with button appearances, that changes every time user click on the button. " +
+                 "First list element is a default button appearance. ")]
+        [FormerlySerializedAs("_buttonAppearances")] 
+        [SerializeField] private List<ButtonAppearance> buttonAppearances;
+
+        [SerializeField] private Image image;
+
+        [SerializeField] private TextMeshProUGUI text;
+
         [SerializeField] private Button button;
-        
-        private int _lastAppearanceIndex = 0;
+
+        private int _lastAppearanceIndex = 1;
 
         private void Awake()
         {
             button.onClick.AddListener(ChangeImageAndText);
         }
         
-        
+        public void SetDefaultAppearance()
+        {
+            if (buttonAppearances != null)
+            {
+                image.sprite = buttonAppearances[0].GetSprite();
+                text.text = buttonAppearances[0].GetText();
+                text.margin = new Vector4(0, 0, 0, buttonAppearances[0].GetOffset());
+                _lastAppearanceIndex++;
+            }
+        }
+
         private void ChangeImageAndText()
         {
-            if (_lastAppearanceIndex > _buttonAppearances.Count - 1)
+            if (_lastAppearanceIndex > buttonAppearances.Count - 1)
             {
                 _lastAppearanceIndex = 0;
             }
-            
-            this._image.sprite = _buttonAppearances[_lastAppearanceIndex].GetSprite();
-            this._text.text = _buttonAppearances[_lastAppearanceIndex].GetText();
-            this._text.margin = new Vector4(0,0,0,  _buttonAppearances[_lastAppearanceIndex].GetOffset());
+
+            image.sprite = buttonAppearances[_lastAppearanceIndex].GetSprite();
+            text.text = buttonAppearances[_lastAppearanceIndex].GetText();
+            text.margin = new Vector4(0, 0, 0, buttonAppearances[_lastAppearanceIndex].GetOffset());
             _lastAppearanceIndex++;
         }
-        
+
         private void OnDestroy()
         {
             button.onClick.RemoveListener(ChangeImageAndText);
