cf1f4c2067e76c3f9f9e5563ba0edd0975e12273
Bartlomiej Stawiarz <extragracz@o2.pl>
2022-01-13 22:43:39 +0100

    Added video file accessor.

---
+++ Assets/_Project/Scripts/MediaAccess/VideoFileAccessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using PolSl.UrbanHealthPath.PathData;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace PolSl.UrbanHealthPath.MediaAccess
+{
+    public class VideoFileAccessor : MediaFileAccessor<VideoClip>
+    {
+        public VideoFileAccessor(MediaFile mediaFile) : base(mediaFile, new [] {MediaFileType.Video})
+        {
+        }
+
+        protected override VideoClip GetLocalMedia()
+        {
+            return Resources.Load<VideoClip>(_mediaFile.Path);
+        }
+
+        protected override VideoClip GetRemoteMedia()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
