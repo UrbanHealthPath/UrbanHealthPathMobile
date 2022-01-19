74fe8b32ec0be4a78db02ac7ed40745d031085bf
Bartlomiej Stawiarz <extragracz@o2.pl>
2022-01-19 23:35:13 +0100

    Whatsapp share.

--- Assets/_Project/Scripts/Controllers/ShareController.cs
+++ Assets/_Project/Scripts/Controllers/ShareController.cs
@@ -1,12 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using VoxelBusters.EssentialKit;
 
 namespace PolSl.UrbanHealthPath.Controllers
 {
     public class ShareController
     {
+        public event Action<string, SocialShareComposerResultCode> WhatsappShared;
+        
         public void ShareWhatsapp(string message)
         {
-            Debug.Log("Shared message " + message);
+            SocialShareComposer composer = SocialShareComposer.CreateInstance(SocialShareComposerType.WhatsApp);
+            composer.SetText(message);
+            composer.SetCompletionCallback((result, error) => {
+                WhatsappShared?.Invoke(message, result.ResultCode);
+            });
+            composer.Show();
         }
     }
 }
