4a8853dbb192b93731821cf3d660a0114e69abfe
Paulina Czapla <paulina.czp00@gmail.com>
2022-01-19 22:36:57 +0100

    Changed QuizWithTexts initialization.

---
+++ Assets/_Project/Scripts/UserInterface/Components/QuizWithTextElementOption.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Events;
+
+namespace PolSl.UrbanHealthPath.UserInterface.Components
+{
+    public class QuizWithTextElementOption
+    {
+        public string Text { get; }
+        
+        public UnityAction<QuizOptionButton> ButtonAction { get; }
+
+        public QuizWithTextElementOption(string text, UnityAction<QuizOptionButton> buttonAction)
+        {
+            Text = text;
+            ButtonAction = buttonAction;
+        }
+    }
+}
