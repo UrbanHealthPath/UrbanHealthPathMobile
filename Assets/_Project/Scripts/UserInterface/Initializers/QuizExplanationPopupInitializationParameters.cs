3aa849c6892d346c77442cdbe41cdc8ca2c1e82f
matadam667 <56017753+matadam667@users.noreply.github.com>
2022-02-13 23:47:35 +0100

    waypoints game explain added, missing file readded.

---
+++ Assets/_Project/Scripts/UserInterface/Initializers/QuizExplanationPopupInitializationParameters.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using PolSl.UrbanHealthPath.UserInterface.Popups;
+using UnityEngine;
+
+namespace PolSl.UrbanHealthPath.UserInterface.Initializers
+{
+    public class QuizExplanationPopupInitializationParameters : IPopupInitializationParameters
+    {
+        public PopupPayload Payload { get;  }
+        public string FirstExplanation { get; }
+        public string SecondExplanation { get; }
+        public string ThirdExplanation { get; }
+        public string FourthExplanation { get; }
+        public Texture FirstTexture { get; }
+        public Texture SecondTexture { get; }
+        public Texture ThirdTexture { get; }
+        public Texture FourthTexture { get; }
+
+        public QuizExplanationPopupInitializationParameters(PopupPayload payload, string firstExplanation,
+            string secondExplanation, string thirdExplanation, string fourthExplanation, Texture firstTexture,
+            Texture secondTexture, Texture thirdTexture, Texture fourthTexture)
+        {
+            Payload = payload;
+            FirstExplanation = firstExplanation;
+            SecondExplanation = secondExplanation;
+            ThirdExplanation = thirdExplanation;
+            FourthExplanation = fourthExplanation;
+            FirstTexture = firstTexture;
+            SecondTexture = secondTexture;
+            ThirdTexture = thirdTexture;
+            FourthTexture = fourthTexture;
+        }
+    }
+}
