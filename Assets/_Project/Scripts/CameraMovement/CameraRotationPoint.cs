73313322d683c4c78e1fe80db5b05395028602ad
Paulina Czapla <paulina.czp00@gmail.com>
2022-02-13 21:54:45 +0100

    Fixed bug with wrong camera's rotation point position.

---
+++ Assets/_Project/Scripts/CameraMovement/CameraRotationPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace PolSl.UrbanHealthPath.CameraMovement
+{
+    public class CameraRotationPoint : MonoBehaviour
+    {
+        [SerializeField] private Transform _player;
+
+        private void FixedUpdate()
+        {
+            if(transform.position != _player.position)
+                transform.position = _player.position;
+        }
+    }
+}
